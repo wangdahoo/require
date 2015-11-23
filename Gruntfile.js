a343e1f511fed0884562b52562b088817f505784
叶俊 <ye.jun@qianka.com>
2015-11-23 13:01:14 +0800

    2.1.22

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'bower_components/requirejs/require.js',
+                dest: '<%= pkg.version %>/require.min.js'
+            }
+        },
+
+        clean: {
+            build: ['<%= pkg.version %>']
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('default', ['clean', 'uglify']);
+
+};
